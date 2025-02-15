94f8bd95458bbffefaaa5ce88b7aac121b309add
pmclainmmllc <patrick.mclain.jr@gmail.com>
2025-02-14 20:20:56 -0600

    updating footer with accreditations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -218,6 +218,32 @@ export default function Home() {
       </main>
 
       <footer className="bg-sage-100 text-white py-8">
+        <div className="container max-w-4xl mx-auto gap-4 flex justify-between items-center mb-4">
+          <Image
+            src="/credits/IAPCP.png"
+            alt="International Alliance Professional Complementary Practictioners Accredited Member"
+            width={267}
+            height={125}
+          />
+          <Image
+            src="/credits/MCH.png"
+            alt="Mindvalley Certified Coach"
+            width={132}
+            height={100}
+          />
+          <Image
+            src="/credits/RHA.png"
+            alt="Reiki Healing Association Member"
+            width={136}
+            height={136}
+          />
+          <Image
+            src="/credits/Regrowth.png"
+            alt="Regrowth Therapy and Healing Logo"
+            width={240}
+            height={87}
+          />
+        </div>
         <div className="container mx-auto px-4 text-center">
           <p>© Regrowth Therapy & Healing LLC. All Rights Reserved</p>
         </div>
