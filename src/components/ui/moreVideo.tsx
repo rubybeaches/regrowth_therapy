7d1acedda82add9e0f14edeaefc126724c23b7e1
pmclainmmllc <patrick.mclain.jr@gmail.com>
2025-02-25 20:52:21 -0600

    add hover affect

--- src/components/ui/moreVideo.tsx
+++ src/components/ui/moreVideo.tsx
@@ -13,6 +13,7 @@ const MoreVideo = ({
       <CardContent className="p-0">
         <Link href={`/${category}`}>
           <div className="aspect-video relative bg-sage-100 flex">
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300 bg-sage-50 bg-opacity-50"></div>
             <h2 className="text-lg text-center text-white m-auto">See More</h2>
           </div>
           <div className="p-4 text-white">
