832b6b1857e40a5dbd7ba4d73f529c70b58c7346
pmclainmmllc <patrick.mclain.jr@gmail.com>
2025-04-04 19:54:09 -0500

    adding timeout for menu display

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,10 +3,29 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { services } from "@/lib/globals";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Header = () => {
   const [showServices, setShowServices] = useState(false);
+  const showMenuRef = useRef<any>();
+
+  const toggleServicMenu = (display: boolean) => {
+    if (showMenuRef) {
+      clearTimer();
+    }
+    setShowServices(display);
+  };
+
+  const setTimer = () => {
+    showMenuRef.current = setTimeout(() => {
+      setShowServices(() => false);
+    }, 1000);
+  };
+
+  const clearTimer = () => {
+    clearTimeout(showMenuRef.current);
+  };
+
   return (
     <header className="bg-sage-100 text-white py-4">
       <div className="container max-w-4xl mx-auto px-4 flex justify-between items-center text-lg">
@@ -23,15 +42,17 @@ const Header = () => {
           <div>
             <Button
               className="bg-sage-100 hover:bg-sage-700 text-white text-base rounded-full"
-              onMouseEnter={() => setShowServices(true)}
+              onMouseEnter={() => toggleServicMenu(true)}
+              onMouseLeave={() => setTimer()}
             >
               Services
             </Button>
             <ul
-              className={`absolute p-6 bg-sage-700 rounded-lg w-[250px] z-[99] ${
+              className={`absolute p-6 bg-sage-700 rounded-lg w-[300px] z-[99] ${
                 showServices ? "block" : "hidden"
               }`}
-              onMouseLeave={() => setShowServices(false)}
+              onMouseEnter={() => clearTimer()}
+              onMouseLeave={() => toggleServicMenu(false)}
             >
               {services.map((service, index) => (
                 <li key={index} className="text-base">
@@ -40,7 +61,10 @@ const Header = () => {
               ))}
             </ul>
           </div>
-          <Button className="bg-sage-100 hover:bg-sage-700 text-white text-base rounded-full">
+          <Button
+            className="bg-sage-100 hover:bg-sage-700 text-white text-base rounded-full"
+            onMouseEnter={() => toggleServicMenu(false)}
+          >
             <Link href="/faq">FAQ</Link>
           </Button>
         </nav>
