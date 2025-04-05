8dd760ecdc66261f77c921405dbb8ed7a381bc3c
pmclainmmllc <patrick.mclain.jr@gmail.com>
2025-04-04 19:18:59 -0500

    getting services menu draft

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,8 +1,12 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { services } from "@/lib/globals";
+import { useState } from "react";
 
 const Header = () => {
+  const [showServices, setShowServices] = useState(false);
   return (
     <header className="bg-sage-100 text-white py-4">
       <div className="container max-w-4xl mx-auto px-4 flex justify-between items-center text-lg">
@@ -15,10 +19,27 @@ const Header = () => {
           />
           <span>Regrowth Therapy & Healing</span>
         </Link>
-        <nav className="flex gap-2">
-          <Button className="bg-sage-100 hover:bg-sage-700 text-white text-base rounded-full">
-            Services
-          </Button>
+        <nav className="flex gap-2 relative">
+          <div>
+            <Button
+              className="bg-sage-100 hover:bg-sage-700 text-white text-base rounded-full"
+              onMouseEnter={() => setShowServices(true)}
+            >
+              Services
+            </Button>
+            <ul
+              className={`absolute p-6 bg-sage-700 rounded-lg w-[250px] z-[99] ${
+                showServices ? "block" : "hidden"
+              }`}
+              onMouseLeave={() => setShowServices(false)}
+            >
+              {services.map((service, index) => (
+                <li key={index} className="text-base">
+                  <Link href={service.link}>{service.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
           <Button className="bg-sage-100 hover:bg-sage-700 text-white text-base rounded-full">
             <Link href="/faq">FAQ</Link>
           </Button>
